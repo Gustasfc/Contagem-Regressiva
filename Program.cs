1c266fa4b926f27b3f94de1d5192410ae5253e89
Gustavo Reis <gustavorgonzalezgja@gmail.com>
2025-03-17 18:37:50 -0300

    tudo certo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.WriteLine("Contagem Regressiva");
+Thread.Sleep(1000);
+
+Console.WriteLine("Preperar...");
+Thread.Sleep(1000);
+
+Console.WriteLine("Apontar...");
+Thread.Sleep(1000);
+
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("Fogoo!");
+Thread.Sleep(1000);
+
+Console.ResetColor();
